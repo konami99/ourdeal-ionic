c9d5b49747b513817aa1b97b8c69f70bf9742981
Richard Chou <rchou@groupon.com>
2015-12-03 17:15:30 +1100

    convert to ts

---
+++ source/models/deal_information_brief.ts
@@ -0,0 +1,13 @@
+module OurDeal {
+	export class DealInformationBrief {
+		DealID: number;	
+		Title: string;
+		PrimaryImage: string;
+	}
+	
+	export class DealInformationDetailed {
+		DealID: number;	
+		Title: string;
+		PrimaryImage: string;
+	}
+}
