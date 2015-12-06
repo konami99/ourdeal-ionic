f2784598108d3029b38b7b28e3c6052de635a282
Richard Chou <rchou@groupon.com>
2015-12-06 23:39:17 +1100

    get deal brief

--- source/controllers/landing_controller.ts
+++ source/controllers/landing_controller.ts
@@ -14,8 +14,9 @@ module OurDeal {
 		constructor(private serchService: ISearchService) {
 			this.serchService.check('https://script.google.com/macros/s/AKfycbza1HDmXJPGvlKozybBVu4OVZkkG4zkMJNp_2skefl9EjyisBrN/exec')
 				.then((result: ng.IHttpPromiseCallbackArg<DealInformationBrief[]>)=>{
-					console.log(result);
+					//console.log(result);
 					this.deals= result.data;
+					console.log(this.deals);
 				});
 		}
 		
--- www/js/app.custom.js
+++ www/js/app.custom.js
@@ -129,8 +129,9 @@ var OurDeal;
             this.serchService = serchService;
             this.serchService.check('https://script.google.com/macros/s/AKfycbza1HDmXJPGvlKozybBVu4OVZkkG4zkMJNp_2skefl9EjyisBrN/exec')
                 .then(function (result) {
-                console.log(result);
+                //console.log(result);
                 _this.deals = result.data;
+                console.log(_this.deals);
             });
         }
         LandingCtrl.$inject = ['SearchService'];
