4d08b442db8e205c4a3167cb53885bf8c165ebc4
Richard Chou <rchou@groupon.com>
2015-12-09 17:14:27 +1100

    get deal brief

---
+++ source/controllers/landing_controller.js
@@ -0,0 +1,31 @@
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-route.d.ts" />
+/// <reference path="../../typings/angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="../../typings/ionic/ionic.d.ts" />
+var OurDeal;
+(function (OurDeal) {
+    'user strict';
+    var LandingCtrl = (function () {
+        function LandingCtrl(serchService, $ionicLoading) {
+            var _this = this;
+            this.serchService = serchService;
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+            this.serchService.check('https://script.google.com/macros/s/AKfycbza1HDmXJPGvlKozybBVu4OVZkkG4zkMJNp_2skefl9EjyisBrN/exec')
+                .then(function (result) {
+                //console.log(result);
+                _this.deals = result.data;
+                console.log(_this.deals);
+            })
+                .finally(function () {
+                $ionicLoading.hide();
+            });
+        }
+        LandingCtrl.$inject = ['SearchService', '$ionicLoading'];
+        return LandingCtrl;
+    })();
+    OurDeal.LandingCtrl = LandingCtrl;
+    angular.module('OurDeal').controller('LandingCtrl', LandingCtrl);
+})(OurDeal || (OurDeal = {}));
+//# sourceMappingURL=landing_controller.js.map
