f0837aeddb99d5b233f095615fb8fa21f5d05e1a
Richard Chou <rchou@groupon.com>
2015-12-14 10:57:56 +1100

    bt integration

---
+++ source/controllers/payment_controller.ts
@@ -0,0 +1,20 @@
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-route.d.ts" />
+/// <reference path="../../typings/angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="../../typings/ionic/ionic.d.ts" />
+
+
+module OurDeal {
+	'user strict';
+	
+	export class PaymentCtrl {
+		//private deals : DealInformationBrief[];
+		
+		//static $inject = ['$stateParams'];
+		constructor() {
+			
+		}
+	}	
+	angular.module('OurDeal').controller('PaymentCtrl', PaymentCtrl);
+
+}
